1c5df865625e6baecaef52c830819e516ce1bc90
Smart  Raysam <engsamtob@yahoo.com>
2023-05-24 12:15:29 +0100

    Add base file for project

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
+
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
+#endif
