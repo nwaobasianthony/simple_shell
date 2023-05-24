42ea493a3038f1febcdf9b7478f3638ed3d457b0
smartraysam <raysamtob@gmail.com>
2023-05-24 22:35:25 +0100

    Resolve  betty issue

--- mem_fun.c
+++ mem_fun.c
@@ -129,21 +129,3 @@ void *_realloc(void *ptr, unsigned int size)
 	return (pointer);
 }
 
-/**
- * _arrdup - safe array duplicate
- * @src: array to be duplicated
- * Return: pointer to pointer of new array
- */
-char **_arrdup(char **src)
-{
-	char **tmp = _malloc(sizeof(char *) * (_arlen(src) + 2));
-	char **_tmp = tmp;
-
-	while (*src)
-		*_tmp = _strdup(*src), _tmp++, src++;
-
-	*_tmp = NULL;
-	return (tmp);
-
-}
-
